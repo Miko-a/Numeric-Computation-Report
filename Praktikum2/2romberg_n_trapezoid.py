0932aa53f788101501e531566887645536a29863
Farrel Jatmiko Aji <176496434+Miko-a@users.noreply.github.com>
2025-06-15 21:45:38 +0700

    Update and rename 2romberg.py to 2romberg_n_trapezoid.py

--- Praktikum2/2romberg_n_trapezoid.py
+++ Praktikum2/2romberg_n_trapezoid.py
@@ -65,3 +65,40 @@ if (a, b) == (0.0, 1.0):
     print("Exact value (π/4): %0.10f" % exact)
     print("Error (Trapezoidal): %0.2e" % abs(exact - trap_result))
     print("Error (Romberg):     %0.2e" % abs(exact - final_romberg))
+
+
+
+
+#################################################################################################################
+
+# Trapezoidal Method
+
+# Define function to integrate
+def f(x):
+    return 1/(1 + x**2)
+
+# Implementing trapezoidal method
+def trapezoidal(x0,xn,n):
+    # calculating step size
+    h = (xn - x0) / n
+    
+    # Finding sum 
+    integration = f(x0) + f(xn)
+    
+    for i in range(1,n):
+        k = x0 + i*h
+        integration = integration + 2 * f(k)
+    
+    # Finding final integration value
+    integration = integration * h/2
+    
+    return integration
+    
+# Input section
+lower_limit = float(input("Enter lower limit of integration: "))
+upper_limit = float(input("Enter upper limit of integration: "))
+sub_interval = int(input("Enter number of sub intervals: "))
+
+# Call trapezoidal() method and get result
+result = trapezoidal(lower_limit, upper_limit, sub_interval)
+print("Integration result by Trapezoidal method is: %0.6f" % (result) )
